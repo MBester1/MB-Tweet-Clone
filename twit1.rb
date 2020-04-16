febd684ba0c519c95cd728489b0240ddc544d119
MBester1 <63454516+MBester1@users.noreply.github.com>
2020-04-16 12:26:16 +0200

    Add files via upload

--- twit1.rb
+++ twit1.rb
@@ -14,32 +14,37 @@ end
 @user1 = User.new("MBester", "falafel1")
 user2 = User.new("Tony_Danza01", "whodaboss!")
 
-
-# @u1_tweets = Array["I just had the best cup of coffee", "What a lovely day!", "I'm quitting social media for a month!"]
-
 def send_tweet
   puts "What's happening?"
   return gets.chomp()
-
 end
 
 def receive_tweet(tweet)
     @user1.tweets << tweet
 end
 
-input = send_tweet()
-receive_tweet(input)
-puts @user1.tweets
-
-
-puts ("Anything new to add " + @user1.username + "?")
-  reply =  gets.chomp()
-while reply != "n"
-  input = send_tweet()
-  receive_tweet(input)
+def view_tweets
   puts @user1.tweets
-  puts ("Anything new to add " + @user1.username + "?")
-  reply =  gets.chomp()
 end
 
-  puts ("See you later. Stay safe!")
+puts "Hey " + @user1.username
+input = send_tweet()
+receive_tweet(input)
+
+reply = ()
+while reply != 3
+  puts ("What would you like to do next?")
+  puts ("1 - Send another tweet")
+  puts ("2 - See all your tweets")
+  puts ("3 - Get outta here")
+  reply =  gets.chomp()
+  if reply == "1"
+    input = send_tweet()
+    receive_tweet(input)
+  elsif  reply == "2"
+  puts @user1.tweets
+  else reply = 3
+  end
+end
+
+puts ("See you later. Stay safe!")
